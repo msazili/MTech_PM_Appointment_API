2ad7251ac465a97ac484a4099e693a81bafa9246
desmong <chongdesmond63@gmail.com>
2021-04-06 21:19:51 +0800

    update

--- Appointment/application.py
+++ Appointment/application.py
@@ -2,6 +2,7 @@ from flask import Flask, make_response, request, jsonify
 from flask_mongoengine import MongoEngine
 from constants import mongodb_password, datetime_format, mongodb_uri, mongodb_name
 import datetime
+import calendar 
 
 app = Flask(__name__)
 
@@ -122,7 +123,7 @@ def api_doctor_spec_data():
         datas = []
         for data in DoctorSpeciality.objects:
             datas.append(data)
-
+        print (type(datas))
         return make_response(jsonify(datas), 200)
 
     elif request.method == "POST":
@@ -155,7 +156,83 @@ def api_each_doctor_spec_data(doctor_id):
         
         return make_response("Deleted successfully", 204)
 
-####################
+##### DoctorRecommendation #####
+@app.route('/api/getDoctorRanking', methods=['POST']) 
+def getDoctorRanking():
+    input_map = request.json
+    patient_id= input_map.get("PatientId")
+    #symptoms = []
+    symptoms= input_map.get("Symptom")
+    date= input_map.get("PreferredDate")
+    time= input_map.get("PreferredTime")
+    #datetime_object = datetime.strptime(available_time, '%m/%d/%Y')
+    
+    #calculate the weekday for particular date
+    def findDay(date): 
+        week1 = datetime.datetime.strptime(date, '%m/%d/%Y').weekday() 
+        return (calendar.day_name[week1])
+
+    Actual_Date= findDay(date)
+    Actual_time = datetime.datetime.strptime(time, '%I:%M%p').time()
+
+    dummy_doctor_profiles = []
+    for dummy_doctor_profiles1 in DoctorSpeciality.objects:
+        dummy_doctor_profiles.append(dummy_doctor_profiles1)
+   
+    # loop doctor profiles and compare with patient symptoms to find the best doctor.
+    # Can assign score for each doctor and rank according using "sorted" function 
+    
+    symptoms_list = set(symptoms.lower().split(","))
+    #Actual_Date1 = list.append(Actual_Date)
+    #Actual_Date1 = set(Actual_Date1.lower())
+    #print(Actual_Date1)
+    print("Patient Symptoms :", symptoms_list)
+    print("Patient available Day :", Actual_Date, "\nPatient available time:", Actual_time)
+    num_of_patient_symptoms= len(symptoms_list)
+    new_doctor_list = []
+    Doctors_time = []
+    Doctors_date = []
+
+    #Get all doctor profile from DB and doing recommendation 
+    for profile in dummy_doctor_profiles:
+        
+        Doctor_date = profile["AvailableDay"]
+        Doctor_time = profile["AvailableTime"]
+        str1 = profile["HandledSymptoms"]
+        handled_symptoms = set(str1.lower().split(","))
+        matched_symptoms= handled_symptoms & symptoms_list
+
+        if matched_symptoms:
+            #symptoms matched and save doctor available date and time for reference if patient have different preference
+            Doctors_date.append((str1 + "     " + Doctor_date + "   " +Doctor_time))
+            Doctor_time = datetime.datetime.strptime(Doctor_time, '%I:%M%p').time()
+            if Actual_Date in Doctor_date:
+                if Actual_time == Doctor_time:
+                    #Score calculation
+                    num_of_matched_symptoms = len(matched_symptoms)
+                    score = float(num_of_matched_symptoms / num_of_patient_symptoms)
+                    print(score)
+                    #higher weightage for matching of most symptoms, score drop as charge rate higher 
+                    profile["Rating"] = ((profile["Rating"]/10 + score*10)/ (profile["ChargeRate"]/5))*10 
+                    new_doctor_list.append(profile)
+                    print ("Doctor Rating : ",profile["Rating"])
+                   
+    ranked_doctor_profiles = sorted(new_doctor_list, key=lambda k: k['Rating'], reverse=True) 
+    #print(ranked_doctor_profiles)
+    #print(Doctors_date)
+
+    if not (ranked_doctor_profiles or Doctors_date):
+        #no matching on symptoms
+        return jsonify('no specialise doctor')
+    elif not ranked_doctor_profiles:
+        #symptoms matched but different timing
+        return jsonify("no suitable time slot!", "Others available doctor time:", Doctors_date)
+    else:
+        #print (ranked_doctor_profiles['score'])
+        filtered_profile  = [ obj for obj in ranked_doctor_profiles if obj['Rating'] > 0.0]
+        return jsonify(filtered_profile)
+
+###################
 
 if __name__ == '__main__':
     app.run()
---
+++ Dockerfile
@@ -0,0 +1,24 @@
+#Use the Python3.6 image
+FROM tiangolo/uwsgi-nginx-flask:python3.6
+
+# Set the working directory to /app
+WORKDIR /Appointment
+
+# Copy server configurations
+COPY nginx.conf .
+COPY uwsgi.ini .
+
+# Copy requirements.txt first to utilise cache
+COPY ./Appointment/requirements.txt .
+
+# Install the dependencies
+#RUN pip install --upgrade certifi
+RUN apt-get update
+RUN apt-get install ca-certificates -y
+RUN update-ca-certificates
+RUN export SSL_CERT_DIR=/etc/ssl/certs
+RUN pip install -r requirements.txt
+
+
+# Copy the current directory contents into the container at /app
+COPY ./Appointment .
